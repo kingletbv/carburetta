6db91ae76693c18f19cee29220faac07485cc5c4
Martijn Boekhorst <martijn@boekhorst.net>
2020-08-18 11:17:03 +0200

    cinder initial commit

---
+++ src/cinder.c
@@ -0,0 +1,31 @@
+#ifndef STDIO_H_INCLUDED
+#define STDIO_H_INCLUDED
+#include <stdio.h>
+#endif
+
+#ifndef STDLIB_H_INCLUDED
+#define STDLIB_H_INCLUDED
+#include <stdlib.h>
+#endif
+
+#ifndef STRING_H_INCLUDED
+#define STRING_H_INCLUDED
+#include <string.h>
+#endif
+
+#ifndef HELPERS_H_INCLUDED
+#define HELPERS_H_INCLUDED
+#include "helpers.h"
+#endif
+
+#ifndef KLT_LOGGER_H_INCLUDED
+#define KLT_LOGGER_H_INCLUDED
+#define KLT_LOG_MODULE "coil"
+#include "klt_logger.h"
+#endif
+
+#ifndef LOG_FUNCTION_H_INCLUDED
+#define LOG_FUNCTION_H_INCLUDED
+#include "log_function.h"
+#endif
+
