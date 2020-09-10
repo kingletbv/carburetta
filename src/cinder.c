5d910f7fb6d0fa89550abf58499ece071fde151f
Martijn Boekhorst <martijn@boekhorst.net>
2020-09-10 11:30:36 +0200

    Use new line_assembly.c/h in favor of line_splitter.c code.

--- src/cinder.c
+++ src/cinder.c
@@ -44,6 +44,11 @@
 #include "tokenizer.h"
 #endif
 
+#ifndef LINE_ASSEMBLY_H_INCLUDED
+#define LINE_ASSEMBLY_H_INCLUDED
+#include "line_assembly.h"
+#endif
+
 #ifndef LINE_SPLITTER_H_INCLUDED
 #define LINE_SPLITTER_H_INCLUDED
 #include "line_splitter.h"
@@ -52,6 +57,11 @@
 #ifndef LINE_DEFS_H_INCLUDED
 #define LINE_DEFS_H_INCLUDED
 #include "line_defs.h"
+#endif
+
+#ifndef DECOMMENT_H_INCLUDED
+#define DECOMMENT_H_INCLUDED
+#include "decomment.h"
 #endif
 
 #ifndef TOKENS_H_INCLUDED
@@ -183,117 +193,91 @@ static int process_cinder_directive(struct tkr_tokenizer *tkr_tokens, struct tkr
   return r;
 }
 
-static int process_cinder_directive2(struct tkr_tokenizer *tkr_tokens, struct tkr_tokenizer *directive_line_match) {
-  char *p = directive_line_match->match_;
 
-  /* Copy to separate buffer to remove line continuations */
-  char *de_lc = (char *)malloc(directive_line_match->token_size_ + 1);
-  if (!de_lc) {
-    LOGERROR("%s(%d): Internal error, no memory\n", directive_line_match->filename_, directive_line_match->start_line_);
-    return TKR_INTERNAL_ERROR;
-  }
-  size_t n = 0;
-  for (p = directive_line_match->match_; p < (directive_line_match->match_ + directive_line_match->token_size_); ++p) {
-    if ((p[0] == '\\') && ((p[1] == '\n') || (p[1] == '\r' && p[2] == '\n'))) {
-      /* Line continuation, eat one char, the for loop will eat the other */
-      p++;
-      if (p[1] == '\r') {
-        /* Eat one more for the CRLF line ending */
-        p++;
+static int process_cinder_directive2(struct tkr_tokenizer *tkr_tokens, struct xlts *directive_line_match) {
+  int r;
+  int ate_percent = 0;
+  int ate_directive = 0;
+  enum {
+    PCD_NT_DIRECTIVE,
+    PCD_TOKEN_DIRECTIVE
+  } directive;
+  tok_switch_to_nonterminal_idents(tkr_tokens);
+
+  r = tkr_tokenizer_input(tkr_tokens, directive_line_match->translated_, directive_line_match->num_translated_, 1);
+  while ((r != TKR_END_OF_INPUT) && (r != TKR_FEED_ME)) {
+    if (r == TKR_SYNTAX_ERROR) {
+      if (isprint(tkr_tokens->match_[0])) {
+        LOGERROR("%s(%d): Character \"%s\" not expected at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, tkr_tokens->match_, directive_line_match->chunks_[0].col_);
+      }
+      else {
+        LOGERROR("%s(%d): Character 0x%02x not expected at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, tkr_tokens->match_[0], directive_line_match->chunks_[0].col_);
       }
     }
-    else {
-      de_lc[n++] = *p;
+    else if (r == TKR_INTERNAL_ERROR) {
+      return r;
     }
-  }
-  de_lc[n] = '\0';
-  p = de_lc;
-
-  const char *line_end = de_lc + n;
-
-  while (p < line_end) {
-    if ((*p == ' ') ||
-        (*p == '\x9') ||
-        (*p == '\xb') ||
-        (*p == '\xc') ||
-        (*p == '\xd')) {
-      p++;
+    else if (r == TKR_MATCH) {
+      if (tkr_tokens->best_match_variant_ == TOK_WHITESPACE) {
+        /* eat silently */
+      }
+      else {
+        if (!ate_percent) {
+          if (tkr_tokens->best_match_variant_ == TOK_PERCENT) {
+            /* leading percent sign indicates cinder directive, eat it */
+            ate_percent = 1;
+          }
+          else {
+            LOGERROR("%s(%d): Syntax error, \"%%\" expected at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, directive_line_match->chunks_[0].col_);
+            return TKR_SYNTAX_ERROR;
+          }
+        }
+        else if (!ate_directive) {
+          if (tkr_tokens->best_match_variant_ == TOK_IDENT) {
+            ate_directive = 1;
+            if (!strcmp("nt", tkr_tokens->match_)) {
+              directive = PCD_NT_DIRECTIVE;
+            }
+            else if (!strcmp("token", tkr_tokens->match_)) {
+              directive = PCD_TOKEN_DIRECTIVE;
+            }
+            else if (!strcmp("debug_end", tkr_tokens->match_)) {
+              LOGERROR("%s(%d): %%debug_end encountered, terminating early\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_);
+              return TKR_INTERNAL_ERROR;
+            }
+            else {
+              LOGERROR("%s(%d): Syntax error, invalid directive at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, directive_line_match->chunks_[0].col_);
+              return TKR_SYNTAX_ERROR;
+            }
+          }
+        }
+        else {
+          if (directive == PCD_NT_DIRECTIVE) {
+            if (tkr_tokens->best_match_variant_ == TOK_IDENT) {
+              TRACE("%s(%d): Non-terminal \"%s\" declared at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, tkr_tokens->match_, directive_line_match->chunks_[0].col_);
+            }
+            else {
+              LOGERROR("%s(%d): Syntax error, identifier expected at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, directive_line_match->chunks_[0].col_);
+              return TKR_SYNTAX_ERROR;
+            }
+          }
+          else if (directive == PCD_TOKEN_DIRECTIVE) {
+            if (tkr_tokens->best_match_variant_ == TOK_IDENT) {
+              TRACE("%s(%d): Token \"%s\" declared at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, tkr_tokens->match_, directive_line_match->chunks_[0].col_);
+            }
+            else {
+              LOGERROR("%s(%d): Syntax error, identifier expected at column %d\n", tkr_tokens->filename_, directive_line_match->chunks_[0].line_, directive_line_match->chunks_[0].col_);
+              return TKR_SYNTAX_ERROR;
+            }
+          }
+        }
+      }
     }
-    else {
-      break;
-    }
-  }
-  if (*p != '%') {
-    LOGERROR("%s(%d): Internal error \"%%\" expected at column %d\n", directive_line_match->filename_, directive_line_match->start_line_, directive_line_match->start_col_ + (int)(p - directive_line_match->match_));
-    free(de_lc);
-    return TKR_INTERNAL_ERROR;
-  }
-  p++;
-  while (p < line_end) {
-    if ((*p == ' ') ||
-        (*p == '\x9') ||
-        (*p == '\xb') ||
-        (*p == '\xc') ||
-        ((*p == '\xd') && (p[1] != '\n'))) {
-      p++;
-    }
-    else {
-      break;
-    }
+
+    r = tkr_tokenizer_input(tkr_tokens, directive_line_match->translated_, directive_line_match->num_translated_, 1);
   }
 
-  if (!isalpha(*p)) {
-    LOGERROR("%s(%d): Syntax error \"%%\" expected at column %d\n", directive_line_match->filename_, directive_line_match->start_line_, directive_line_match->start_col_ + (int)(p - directive_line_match->match_));
-    free(de_lc);
-    return TKR_SYNTAX_ERROR;
-  }
-
-  const char *cmd_start = p;
-  if (isalpha(*p) || (*p == '_') || (*p == '-')) {
-    do {
-      p++;
-    } while (isalpha(*p) || isdigit(*p) || (*p == '_') || (*p == '-'));
-  }
-  const char *cmd_end = p;
-
-  while (p < line_end) {
-    if ((*p == ' ') ||
-      (*p == '\x9') ||
-      (*p == '\xb') ||
-      (*p == '\xc') ||
-      ((*p == '\xd') && (p[1] != '\n'))) {
-      p++;
-    }
-    else {
-      break;
-    }
-  }
-
-  char *value_start = p;
-  while ((p < line_end) && !((*p == '\n') || ((*p == '\r') && (p[1] == '\n')))) {
-    p++;
-  }
-  char *value_end = p;
-
-#define MATCH(id) (strlen(id) == (cmd_end - cmd_start)) && !memcmp(cmd_start, id, cmd_end - cmd_start)
-
-  if (MATCH("nt")) {
-    char savec = *value_end;
-    *value_end = '\0';
-    LOG("%s(%d): \"%s\" (at column %d) recognized as non-terminal\n", directive_line_match->filename_, directive_line_match->start_line_, value_start, directive_line_match->col_);
-    *value_end = savec;
-  }
-  else if (MATCH("debug_end")) {
-    LOGERROR("%s(%d): %%debug_end encountered, terminating early\n", directive_line_match->filename_, directive_line_match->start_line_);
-    free(de_lc);
-    return TKR_INTERNAL_ERROR;
-  }
-
-#undef MATCH
-
-  free(de_lc);
-
-  return 0;
+  return r;
 }
 
 static int process_tokens(struct tkr_tokenizer *tkr_tokens, struct tkr_tokenizer *tkr_lines, int is_final, struct prd_stack *prds) {
@@ -383,6 +367,19 @@ int main(int argc, char **argv) {
     LOGERROR("Failed to initialize tok\n");
     return EXIT_FAILURE;
   }
+  r = las_init();
+  if (r) {
+    LOGERROR("Failed to initialize las\n");
+    return EXIT_FAILURE;
+  }
+  r = dct_init();
+  if (r) {
+    LOGERROR("Failed to initialize dct\n");
+    return EXIT_FAILURE;
+  }
+
+  struct las_line_assembly line_assembly;
+  las_init_line_assembly(&line_assembly);
 
   struct ls_line_splitter line_splitter;
   ls_init_line_splitter(&line_splitter);
@@ -423,9 +420,131 @@ int main(int argc, char **argv) {
 
   size_t num_bytes_read;
   static char buf[2400];
+
+  struct xlts token_buf;
+  xlts_init(&token_buf);
+
   do {
     num_bytes_read = fread(buf, sizeof(*buf), sizeof(buf) / sizeof(*buf), fp);
 
+#if 1
+    r = las_input(&line_assembly, buf, num_bytes_read, !num_bytes_read);
+    while ((r != LAS_END_OF_INPUT) && (r != LAS_FEED_ME)) {
+      /* Cannot modify (shift) the line_assembly's mlc_buf line buffer, so copy it over to a work area (token_buf) */
+      xlts_reset(&token_buf);
+      r = xlts_append(&token_buf, &line_assembly.mlc_buf_);
+      if (r) {
+        return EXIT_FAILURE;
+      }
+
+      dct_decomment(&token_buf);
+
+      r = tkr_tokenizer_input(&tkr_lines, token_buf.translated_, token_buf.num_translated_, 1);
+
+      if ((r == TKR_END_OF_INPUT) || (r == TKR_FEED_ME)) {
+        LOGERROR("%s(%d): Internal error: all lines are expected to match.\n");
+        return EXIT_FAILURE;
+      }
+      if (r == TKR_SYNTAX_ERROR) {
+        if (isprint(tkr_lines.match_[0])) {
+          LOGERROR("%s(%d): Character \"%s\" not expected at column %d\n", tkr_lines.filename_, tkr_lines.best_match_line_, tkr_lines.match_, tkr_lines.best_match_col_);
+        }
+        else {
+          LOGERROR("%s(%d): Character 0x%02x not expected at column %d\n", tkr_lines.filename_, tkr_lines.best_match_line_, tkr_lines.match_[0], tkr_lines.best_match_col_);
+        }
+      }
+      else if (r == TKR_INTERNAL_ERROR) {
+        num_bytes_read = 0;
+        break;
+      }
+
+      if (r == TKR_MATCH) {
+        printf("Match %s: ", ld_line_type_to_str(tkr_lines.best_match_action_));
+        printf("%s", tkr_lines.match_);
+        if (tkr_lines.match_[tkr_lines.token_size_ - 1] != '\n') {
+          /* Last line of input has no trailing newline */
+          printf("\n");
+        }
+        if (where_are_we == PROLOGUE) {
+          switch (tkr_lines.best_match_variant_) {
+          case LD_C_PREPROCESSOR:
+            /* Preserve line continuations */
+            append_part(&prologue, token_buf.num_original_, token_buf.original_);
+            break;
+          case LD_CINDER_SECTION_DELIMITER:
+            where_are_we = GRAMMAR;
+            break;
+          case LD_REGULAR:
+            /* XXX: Process this by individual tokens later ? */
+            /* Preserve line continuations */
+            append_part(&prologue, token_buf.num_original_, token_buf.original_);
+            printf("%s", token_buf.original_);
+            break;
+          case LD_CINDER_DIRECTIVE:
+            r = process_cinder_directive2(&tkr_tokens, &token_buf);
+            if (r == TKR_INTERNAL_ERROR) {
+              return EXIT_FAILURE;
+            }
+            break;
+          }
+        }
+        else if (where_are_we == GRAMMAR) {
+          switch (tkr_lines.best_match_variant_) {
+          case LD_C_PREPROCESSOR:
+            LOGERROR("%s(%d): Error, preprocessor directives do not belong in grammar area\n", tkr_lines.filename_, tkr_lines.start_line_);
+            return EXIT_SUCCESS;
+          case LD_CINDER_SECTION_DELIMITER:
+            /* Finish up */
+            r = process_tokens(&tkr_tokens, &tkr_lines, 1, &prds);
+            where_are_we = EPILOGUE;
+            break;
+          case LD_REGULAR:
+          {
+            r = process_tokens(&tkr_tokens, &tkr_lines, 0, &prds);
+            break;
+          }
+          case LD_CINDER_DIRECTIVE:
+            r = process_cinder_directive2(&tkr_tokens, &token_buf);
+            if (r == TKR_INTERNAL_ERROR) {
+              return EXIT_FAILURE;
+            }
+            break;
+          }
+        }
+        else /* (where_are_we == EPILOGUE) */ {
+          switch (tkr_lines.best_match_variant_) {
+          case LD_C_PREPROCESSOR:
+            append_part(&prologue, line_splitter.num_original_, line_splitter.original_);
+            break;
+          case LD_CINDER_SECTION_DELIMITER:
+            where_are_we = GRAMMAR;
+            break;
+          case LD_REGULAR:
+            /* XXX: Process this by individual tokens later ? */
+            /* Preserve line continuations */
+            append_part(&prologue, line_splitter.num_original_, line_splitter.original_);
+            printf("%s", line_splitter.original_);
+            break;
+          case LD_CINDER_DIRECTIVE:
+            r = process_cinder_directive2(&tkr_tokens, &token_buf);
+            if (r == TKR_INTERNAL_ERROR) {
+              return EXIT_FAILURE;
+            }
+            break;
+          }
+        }
+      }
+
+      r = tkr_tokenizer_input(&tkr_lines, token_buf.translated_, token_buf.num_translated_, 1);
+      if ((r != TKR_END_OF_INPUT) && (r != TKR_INTERNAL_ERROR)) {
+        LOGERROR("%s(%d): Internal error: lines from a single line are expected to match a single time.\n");
+        return EXIT_FAILURE;
+      }
+
+      r = las_input(&line_assembly, buf, num_bytes_read, !num_bytes_read);
+    }
+
+#else
     r = ls_input(&line_splitter, buf, num_bytes_read, !num_bytes_read);
     while ((r != LSSL_END_OF_INPUT) && (r != LSSL_FEED_ME)) {
 
@@ -547,10 +666,14 @@ int main(int argc, char **argv) {
       r = ls_input(&line_splitter, buf, num_bytes_read, !num_bytes_read);
     }
 
+#endif
+
   } while (num_bytes_read);
 
   /* Finish */
   r = process_tokens(&tkr_tokens, &tkr_lines, 0, &prds);
+
+  xlts_cleanup(&token_buf);
 
   prd_stack_cleanup(&prds);
 
@@ -560,6 +683,10 @@ int main(int argc, char **argv) {
 
   ls_cleanup_line_splitter(&line_splitter);
 
+  las_cleanup_line_assembly(&line_assembly);
+
+  dct_cleanup();
+  las_cleanup();
   tok_cleanup();
   ldl_cleanup();
   prd_cleanup();
