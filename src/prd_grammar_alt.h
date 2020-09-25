bb1d7a0782a79fcfae254e6dcaa54760186c7a31
Martijn Boekhorst <martijn@boekhorst.net>
2020-09-25 12:01:14 +0200

    prd_grammar_alt.h: generated header

---
+++ src/prd_grammar_alt.h
@@ -0,0 +1,25 @@
+#ifndef CINDER_PRD_PRD_GRAMMAR_ALT_H_INCLUDED
+#define CINDER_PRD_PRD_GRAMMAR_ALT_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PRD_IDENT 3
+#define PRD_COLON 4
+#define PRD_EQUALS 5
+#define PRD_SEMICOLON 6
+#define PRD_TOKEN 7
+#define PRD_PAR_OPEN 8
+#define PRD_PAR_CLOSE 9
+#define PRD_CUBRACE_OPEN 10
+#define PRD_CUBRACE_CLOSE 11
+#define PRD_ERROR 12
+#define PRD_INPUT_END 13
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* CINDER_PRD_PRD_GRAMMAR_ALT_H_INCLUDED */
