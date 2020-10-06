a966de324c2346db49902dc611a07a0616b361db
Martijn Boekhorst <martijn@boekhorst.net>
2020-10-06 16:10:14 +0200

    prd_grammar_alt.c: update comments and license header.

--- src/prd_grammar_alt.c
+++ src/prd_grammar_alt.c
@@ -1,3 +1,18 @@
+/* Copyright 2020 Kinglet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http ://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef STDIO_H_INCLUDED
 #define STDIO_H_INCLUDED
 #include <stdio.h>
@@ -48,7 +63,10 @@
 #include "prd_gram.h"
 #endif
 
+#ifndef PRD_GRAMMAR_ALT_H_INCLUDED
+#define PRD_GRAMMAR_ALT_H_INCLUDED
 #include "prd_grammar_alt.h"
+#endif
 
 
 
@@ -58,7 +76,7 @@
 
 
 
-/* --------- HERE GOES THE GENERATED FLUFF ------------ */
+/* --------- START OF GENERATED CODE ------------ */
 #include <stdlib.h> /* realloc(), free(), NULL, size_t */
 struct prd_sym_data {
   int state_;
@@ -945,7 +963,7 @@ int prd_parse(struct prd_stack *stack, int sym, struct prd_grammar *g, struct tk
     } /* stack->error_recovery_ */
   } /* for (;;) */
 }
-/* --------- HERE ENDS THE GENERATED FLUFF ------------ */
+/* --------- END OF GENERATED CODE ------------ */
 
 int prd_parse_tkr(struct prd_stack *stack, struct prd_grammar *g, struct tkr_tokenizer *tkr, int end_of_input, struct symbol_table *st) {
   int sym;
