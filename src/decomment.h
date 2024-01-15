8064228d132358fe7660ee30a73e5089d7706b14
Martijn Boekhorst <martijn@boekhorst.net>
2024-01-15 10:25:58 +0100

    bump copyright headers to 2024 ...
    ... happy new year!

--- src/decomment.h
+++ src/decomment.h
@@ -1,4 +1,4 @@
-/* Copyright 2020-2023 Kinglet B.V.
+/* Copyright 2020-2024 Kinglet B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/version.h
+++ src/version.h
@@ -1,3 +1,18 @@
+/* Copyright 2020-2024 Kinglet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef VERSION_H
 #define VERSION_H
 
@@ -19,7 +34,7 @@ extern "C" {
 #define CARBURETTA_VERSION_STR CARBURETTA_STRINGIFY(CARBURETTA_MAJOR_VER) "." CARBURETTA_STRINGIFY(CARBURETTA_MINOR_VER) "." CARBURETTA_STRINGIFY(CARBURETTA_PATCH_VER)
 #endif
 
-#define CARBURETTA_COPYRIGHT_STR "(C) 2020-2023 Kinglet B.V."
+#define CARBURETTA_COPYRIGHT_STR "(C) 2020-2024 Kinglet B.V."
 
 #ifdef __cplusplus
 } /* extern "C" */
