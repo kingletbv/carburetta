8df685f1583d828316d538024b8ac3b4c83a72ef
Martijn Boekhorst <martijn@boekhorst.net>
2026-04-07 06:36:56 +0200

    fix: missing NULL check

--- examples/kc/src/c_compiler.c
+++ examples/kc/src/c_compiler.c
@@ -284,6 +284,7 @@ static int cc_file_mem_input_request_callback(void *baton, struct input_file *if
 
 struct input_file *cc_push_input_file_mem(struct c_compiler *cc, const char *filename, const void *mem, size_t memsize) {
   struct mem_input_file *ifile = (struct mem_input_file *)cc_push_input_file(cc, filename, sizeof(struct mem_input_file) - sizeof(struct input_file));
+  if (ifile) return NULL;
   ifile->if_.input_request_fn_ = cc_file_mem_input_request_callback;
   ifile->mem_ = mem;
   ifile->memsize_ = memsize;
